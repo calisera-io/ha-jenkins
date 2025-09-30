ce5f47b51e58651a42676c46c88e72997dda8a7f
hogh@calisera.io <hogh@calisera.io>
2025-09-30 16:41:25 +0200

    update script for github webhook secret

--- setup-github-webhook-secret.sh
+++ setup-github-webhook-secret.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-read -s -p "Enter Github token: " GITHUB_WEBHOOK_SECRET
-echo
-read -s -p "Confirm Github token: " GITHUB_WEBHOOK_SECRET_CONFIRMATION
-echo
-
-if [ "$GITHUB_WEBHOOK_SECRET" != "$GITHUB_WEBHOOK_SECRET_CONFIRMATION" ]; then
-    echo "Error: Tokens do not match."
-    exit 1
-fi
+GITHUB_WEBHOOK_SECRET=$(openssl rand -base64 32)
 
 aws ssm put-parameter \
   --name "/jenkins/dev/github_webhook_secret" \
   --value "$GITHUB_WEBHOOK_SECRET" \
   --type "SecureString" \
   --overwrite
+
+echo "$GITHUB_WEBHOOK_SECRET"
